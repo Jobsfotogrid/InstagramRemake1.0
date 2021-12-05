520a9b3cdce0b0c2ad0fa3438b85f5289750dba7
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-05 13:50:15 -0300

    Validação de Formulário

--- app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
@@ -3,12 +3,16 @@ package br.com.instagram.login.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.instagram.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextInputLayout>(R.id.login_edit_email_input)
+            .error = "Esse e-mail é inválido"
     }
 
 }
