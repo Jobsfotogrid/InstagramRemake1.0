7c8f89d6a09a72166d0e9483175621a93acebbc8
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-05 13:32:36 -0300

    Início do Projeto: Desenhando Tela de Login

---
+++ app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
@@ -0,0 +1,14 @@
+package br.com.instagram.login.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.instagram.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
