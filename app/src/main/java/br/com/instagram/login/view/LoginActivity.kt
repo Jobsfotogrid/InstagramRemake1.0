dd31519ff07833df672ab75811acb86aed039bbf
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-06 06:32:55 -0300

    TextWatcher: Alterando Formulário com Inputs

--- app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
@@ -1,8 +1,12 @@
 package br.com.instagram.login.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import br.com.instagram.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
@@ -11,8 +15,35 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<TextInputLayout>(R.id.login_edit_email_input)
-            .error = "Esse e-mail é inválido"
+        val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+        editTextEmail.addTextChangedListener(watcher)
+        editTextPassword.addTextChangedListener(watcher)
+
+        findViewById<Button>(R.id.login_btn_enter).setOnClickListener {
+
+            findViewById<TextInputLayout>(R.id.login_edit_email_input)
+                .error = "Esse e-mail é inválido"
+
+            findViewById<TextInputLayout>(R.id.login_edit_password_input)
+                .error = "Senha incorreta"
+
+        }
+    }
+
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            findViewById<Button>(R.id.login_btn_enter).isEnabled = p0.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
     }
 
 }
