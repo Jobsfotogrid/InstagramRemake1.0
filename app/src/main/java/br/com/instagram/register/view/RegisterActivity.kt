2593fca3c776d3fb01dbf59b2b9fb353a08ff18d
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-06 08:18:02 -0300

    Fragmentos e Tela de Cadastro Com Email

---
+++ app/src/main/java/br/com/instagram/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package br.com.instagram.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.instagram.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
