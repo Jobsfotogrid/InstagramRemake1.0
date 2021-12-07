e5895611a231d76a3694dc092caadc4b196298a6
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-07 15:06:49 -0300

    Toolbar Customizada e Com Menus

--- app/src/main/java/br/com/instagram/main/view/MainActivity.kt
+++ app/src/main/java/br/com/instagram/main/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import br.com.instagram.R
 
@@ -21,6 +22,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
 
 }
