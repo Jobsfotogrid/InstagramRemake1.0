34e04483b57818a918ccc4d251daa041d2f40467
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-15 18:17:02 -0300

    Pré-visualização de Câmera

--- app/src/main/java/br/com/instagram/add/view/AddFragment.kt
+++ app/src/main/java/br/com/instagram/add/view/AddFragment.kt
@@ -2,14 +2,16 @@ package br.com.instagram.add.view
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import br.com.instagram.R
 import br.com.instagram.add.Add
 import br.com.instagram.common.base.BaseFragment
 import br.com.instagram.databinding.FragmentAddBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
@@ -29,6 +31,20 @@ class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
         viewPager?.adapter = adapter
 
         if (tabLayout != null && viewPager != null) {
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    if (tab?.text == getString(adapter.tabs[0])) {
+                        startCamera()
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                }
+
+            })
             TabLayoutMediator(tabLayout, viewPager) { tab, position ->
                 tab.text = getString(adapter.tabs[position])
             }.attach()
@@ -42,20 +58,27 @@ class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
     }
 
     private fun startCamera() {
-        // TODO:
-        Log.i("Teste", "startCamera")
+        setFragmentResult("cameraKey", bundleOf("startCamera" to true))
     }
 
-    private val getPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            Toast.makeText(requireContext(), R.string.permission_camera_denied, Toast.LENGTH_LONG).show()
+    private val getPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    R.string.permission_camera_denied,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
-    }
 
     private fun allPermissionsGranted() =
-        ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
 
     companion object {
         private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
