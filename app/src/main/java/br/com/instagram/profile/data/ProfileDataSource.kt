8e04ae6d282c802e9b1e649348e2663bb815a629
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-17 20:03:23 -0300

    Ação de Seguir e Seguindo + Conclusão do Módulo

--- app/src/main/java/br/com/instagram/profile/data/ProfileDataSource.kt
+++ app/src/main/java/br/com/instagram/profile/data/ProfileDataSource.kt
@@ -3,12 +3,15 @@ package br.com.instagram.profile.data
 import br.com.instagram.common.base.RequestCallback
 import br.com.instagram.common.model.Post
 import br.com.instagram.common.model.UserAuth
+import java.lang.UnsupportedOperationException
 
 interface ProfileDataSource {
 
     fun fetchUserProfile(userUUID: String, callback: RequestCallback<Pair<UserAuth, Boolean?>>)
 
     fun fetchUserPosts(userUUID: String, callback: RequestCallback<List<Post>>)
+
+    fun followUser(userUUID: String, isFollow: Boolean, callback: RequestCallback<Boolean>) { throw UnsupportedOperationException() }
 
     fun fetchSession() : UserAuth { throw UnsupportedOperationException() }
 
