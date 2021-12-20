107161e57e0041abbea894253aea24ed271e0dcb
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-20 18:48:11 -0300

    Contador de Seguidores

--- app/src/main/java/br/com/instagram/profile/data/FireProfileDataSource.kt
+++ app/src/main/java/br/com/instagram/profile/data/FireProfileDataSource.kt
@@ -91,6 +91,8 @@ class FireProfileDataSource : ProfileDataSource {
                 else FieldValue.arrayRemove(uid)
             )
             .addOnSuccessListener { res ->
+                followingCounter(uid, isFollow)
+                followersCounter(userUUID)
                 callback.onSuccess(true)
             }
             .addOnFailureListener { exception ->
@@ -106,6 +108,8 @@ class FireProfileDataSource : ProfileDataSource {
                             )
                         )
                         .addOnSuccessListener { res ->
+                            followingCounter(uid, isFollow)
+                            followersCounter(userUUID)
                             callback.onSuccess(true)
                         }
                         .addOnFailureListener { exception ->
@@ -118,6 +122,33 @@ class FireProfileDataSource : ProfileDataSource {
             .addOnCompleteListener {
                 callback.onComplete()
             }
+    }
+
+    private fun followingCounter(uid: String, isFollow: Boolean) {
+        val meRef = FirebaseFirestore.getInstance()
+            .collection("/users")
+            .document(uid)
+
+        if (isFollow) meRef.update("following", FieldValue.increment(1))
+        else meRef.update("following", FieldValue.increment(-1))
+    }
+
+    private fun followersCounter(uid: String) {
+        val meRef = FirebaseFirestore.getInstance()
+            .collection("/users")
+            .document(uid)
+
+        FirebaseFirestore.getInstance()
+            .collection("/followers")
+            .document(uid)
+            .get()
+            .addOnSuccessListener { response ->
+                if (response.exists()) {
+                    val list = response.get("followers") as List<String>
+                    meRef.update("followers", list.size)
+                }
+            }
+
     }
 
 }
