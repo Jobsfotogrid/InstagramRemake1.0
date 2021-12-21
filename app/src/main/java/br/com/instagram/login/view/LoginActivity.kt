6da92c064f69d3e0ea7d8e063814130132d454dc
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-20 22:04:04 -0300

    Adicionando Modo Escuro - Dark Mode + Conclusão

--- app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/instagram/login/view/LoginActivity.kt
@@ -1,9 +1,15 @@
 package br.com.instagram.login.view
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowInsetsController
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import br.com.instagram.R
 import br.com.instagram.common.base.DependencyInjector
 import br.com.instagram.common.util.TxtWatcher
 import br.com.instagram.databinding.ActivityLoginBinding
@@ -30,6 +36,16 @@ class LoginActivity : AppCompatActivity(), Login.View {
         presenter = LoginPresenter(this, DependencyInjector.loginRepository())
 
         with(binding) {
+
+            when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    loginImgLogo.imageTintList = ColorStateList.valueOf(Color.WHITE)
+                }
+                Configuration.UI_MODE_NIGHT_NO -> {
+
+                }
+            }
+
             loginEditEmail.addTextChangedListener(watcher)
             loginEditEmail.addTextChangedListener(TxtWatcher {
                 displayEmailFailure(null)
@@ -86,4 +102,5 @@ class LoginActivity : AppCompatActivity(), Login.View {
     override fun onUserUnauthorized(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
+
 }
--- app/src/main/java/br/com/instagram/register/view/RegisterEmailFragment.kt
+++ app/src/main/java/br/com/instagram/register/view/RegisterEmailFragment.kt
@@ -1,6 +1,9 @@
 package br.com.instagram.register.view
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -28,6 +31,15 @@ class RegisterEmailFragment : Fragment(R.layout.fragment_register_email), Regist
 
         binding?.let {
             with(it) {
+                when (resources.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        registerImgLogo.imageTintList = ColorStateList.valueOf(Color.WHITE)
+                    }
+                    Configuration.UI_MODE_NIGHT_NO -> {
+
+                    }
+                }
+
                 registerTxtLogin.setOnClickListener {
                     activity?.finish()
                 }
@@ -62,7 +74,8 @@ class RegisterEmailFragment : Fragment(R.layout.fragment_register_email), Regist
     }
 
     private val watcher = TxtWatcher {
-        binding?.registerBtnNext?.isEnabled = binding?.registerEditEmail?.text.toString().isNotEmpty()
+        binding?.registerBtnNext?.isEnabled =
+            binding?.registerEditEmail?.text.toString().isNotEmpty()
     }
 
     override fun showProgress(enabled: Boolean) {
